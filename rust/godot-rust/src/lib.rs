0d059f448c6f11eab9cee91a2ee3d730d7f72ef4
Tamego <oitrosutuik@gmail.com>
2025-09-28 00:23:36 +0900

    fix(godot-rust): align entangled lib path with module source

--- rust/godot-rust/src/lib.rs
+++ rust/godot-rust/src/lib.rs
@@ -1,9 +1,9 @@
-// ~/~ begin <<rust/src/lib.md#rust/src/lib.rs>>[init]
-// ~/~ begin <<rust/src/lib.md#l_godot-rust-module>>[init]
+// ~/~ begin <<rust/godot-rust/src/lib.md#rust/godot-rust/src/lib.rs>>[init]
+// ~/~ begin <<rust/godot-rust/src/lib.md#l_godot-rust-module>>[init]
 use godot::prelude::*;
 // ~/~ end
 
-// ~/~ begin <<rust/src/lib.md#l_gdextension-entry-point>>[init]
+// ~/~ begin <<rust/godot-rust/src/lib.md#l_gdextension-entry-point>>[init]
 struct ViskaSATExtension;
 
 #[gdextension]
